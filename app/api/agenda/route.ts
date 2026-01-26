ed40808b5b6a388eb74841b15719104a009ca522
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-26 15:30:33 -0300

    Create route.ts

---
+++ app/api/agenda/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const res = await fetch(process.env.GOOGLE_SCRIPT_URL!)
+  const data = await res.json()
+  return NextResponse.json(data)
+}
