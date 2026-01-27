c2770ff05439d07d088c575e86dc2e1bc1c9a6b0
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-26 23:21:20 -0300

    Atualizar o route.ts

--- app/api/agenda/route.ts
+++ app/api/agenda/route.ts
@@ -1,17 +1,19 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
+    const auth = new JWT({
+      email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
     const doc = new GoogleSpreadsheet(
       process.env.GOOGLE_SHEET_ID!,
-      undefined
+      auth
     );
-
-    await doc.useServiceAccountAuth({
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
-      private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-    });
 
     await doc.loadInfo();
 
