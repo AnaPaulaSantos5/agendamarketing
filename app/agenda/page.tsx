4f165797adca99614af6540495a1293b707b5ac0
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-27 21:14:07 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
 import AgendaCalendar from '../components/AgendaCalendar';
 
 export default function AgendaPage() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Agenda de Conteúdos</h1>
-      <AgendaCalendar />
-    </div>
-  );
+  return <AgendaCalendar />;
 }
