d18c32707075a7756ce9624db3a269e815c94938
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-28 22:03:25 -0300

    Atualizar o AgendaCalendar.tsx

--- app/components/AgendaCalendar.tsx
+++ app/components/AgendaCalendar.tsx
@@ -5,7 +5,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
-import { AgendaEvent } from '../app/types'
+import { AgendaEvent } from '../types/agenda'
 import EventModal from './EventModal'
 
 export default function AgendaCalendar() {
