3a01f08dfefce79b98a41c3d4111c76dd020e2a4
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-28 22:51:03 -0300

    Criar googleSheets.ts

---
+++ lib/googleSheets.ts
@@ -0,0 +1,13 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+export async function getDoc() {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID!);
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  });
+
+  await doc.loadInfo();
+  return doc;
+}
