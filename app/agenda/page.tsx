5b9d19d599c648953994ee96af0e2dc41970498d
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-28 21:27:24 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,5 +1,14 @@
-import AgendaCalendar from '../components/AgendaCalendar';
+import AgendaCalendar from '@/app/components/AgendaCalendar'
+import ChecklistLateral from '@/app/components/ChecklistLateral'
 
 export default function AgendaPage() {
-  return <AgendaCalendar />;
+  return (
+    <div className="flex h-screen">
+      <div className="flex-1">
+        <AgendaCalendar />
+      </div>
+
+      <ChecklistLateral />
+    </div>
+  )
 }
