fe10467100a3a5e3fee3645e7e3447719c5cb5e7
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-28 23:19:08 -0300

    Atualizar o types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,29 @@
-export type Perfil = 'Confi' | 'Cecília' | 'Luiza' | 'Júlio' | 'Confi Finanças' | 'Confi Benefícios';
+export type Perfil =
+  | 'Confi'
+  | 'Cecília'
+  | 'Luiza'
+  | 'Júlio'
+  | 'Confi Finanças'
+  | 'Confi Benefícios'
+  | 'Confi Seguros';
 
 export type AgendaEvent = {
   id: string;
-  title: string;
   start: string;
   end: string;
   tipoEvento: string;
   tipo: string;
   conteudoPrincipal: string;
-  conteudoSecundario: string;
-  cta: string;
-  statusPostagem: string;
-  perfil: Perfil; // Agora todas as opções são aceitas
-  checklist?: string[];
+  conteudoSecundario?: string;
+  cta?: string;
+  statusPostagem?: string;
+  perfil: Perfil;
+  tarefa?: {
+    titulo: string;
+    responsavel: Perfil;
+    data: string;
+    status: string;
+    linkDrive?: string;
+    notificar?: string;
+  };
 };
