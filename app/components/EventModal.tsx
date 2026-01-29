3df8c617116af7970ccd50711a08c9d2bbb9485d
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-28 23:36:08 -0300

    Atualizar o EventModal.tsx

--- app/components/EventModal.tsx
+++ app/components/EventModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import { AgendaEvent, Perfil } from '@/lib/types';
+import { AgendaEvent, Perfil } from '../types';
 import { v4 as uuid } from 'uuid';
 
 type Props = {
