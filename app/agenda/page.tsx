8de93a38e98af8b8c83801c75a7e372b16fce8f8
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-29 20:54:26 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,5 +1,7 @@
 import AgendaCalendar from '../components/AgendaCalendar';
+// Aqui você traz os dados da planilha (exemplo mock)
+import { sheetDataMock } from '../data/mockSheet';
 
 export default function AgendaPage() {
-  return <AgendaCalendar />;
+  return <AgendaCalendar sheetData={sheetDataMock} />;
 }
