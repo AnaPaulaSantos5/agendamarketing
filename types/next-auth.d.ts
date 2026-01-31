247a6bc697eb8a70e84451a5658bd8473912ce91
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-31 19:10:48 -0300

    Atualizar o next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,19 @@
+// types/next-auth.d.ts
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-      perfil: "Luiza" | "Cecília" | "Júlio" | "Confi";
+      name?: string;
+      email?: string;
+      image?: string;
+      perfil: "Confi" | "Cecília" | "Luiza" | "Júlio";
       responsavelChatId: string;
     };
+  }
+
+  interface JWT {
+    perfil?: "Confi" | "Cecília" | "Luiza" | "Júlio";
+    responsavelChatId?: string;
   }
 }
