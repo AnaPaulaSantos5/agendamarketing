c9c1879c938f8fa8cd505c345338e709599d6c68
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-01-31 22:14:26 -0300

    Atualizar o EventModal.tsx

--- app/components/EventModal.tsx
+++ app/components/EventModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { AgendaEvent, Perfil } from './AgendaCalendar';
+import { AgendaEvent, Perfil } from './types';
 
 type Props = {
   isOpen: boolean;
