1c6baeb5f87bc5910b93b9274dac4e85a02d5bcc
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-01 12:08:18 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,31 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import AgendaCalendar from '../components/AgendaCalendar';
-import { AgendaEvent, Perfil } from '../components/types';
 
 export default function AgendaPage() {
-  const [events, setEvents] = useState<AgendaEvent[]>([]);
-
-  useEffect(() => {
-    async function loadAgenda() {
-      const res = await fetch('/api/agenda');
-      const data = await res.json();
-      setEvents(data);
-    }
-    loadAgenda();
-  }, []);
-
-  return (
-    <AgendaCalendar
-      events={events}
-      userPerfil={'Confi'}
-      onDateSelect={(start, end) => {
-        console.log('Selecionado:', start, end);
-      }}
-      onEventClick={(ev) => {
-        console.log('Evento clicado:', ev);
-      }}
-    />
-  );
+  return <AgendaCalendar />;
 }
