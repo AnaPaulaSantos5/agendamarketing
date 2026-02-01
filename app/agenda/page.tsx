f5a39b57df3004ed979fd195f19dd1ae04182181
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-01 11:20:31 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import AgendaLayout from '../components/AgendaLayout';
 import { AgendaEvent, ChecklistItem, Perfil } from '../components/types';
-import { getEvents, saveEvent, deleteEvent } from '@/services/agendaService';
+import { getEvents, saveEvent, deleteEvent } from '../../agendaService';
 
 export default function AgendaPage() {
   const [events, setEvents] = useState<AgendaEvent[]>([]);
