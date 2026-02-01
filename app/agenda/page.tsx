43d4190ef4faf736d68c2c3295d1302af9f00623
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-01 14:41:16 -0300

    Atualizar o page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,23 +1,15 @@
+// app/agenda/page.tsx
 'use client';
 
 import AgendaCalendar from '../components/AgendaCalendar';
-import { useSession, signIn } from 'next-auth/react';
+import ProtectedRoute from '../components/ProtectedRoute';
+import Header from '../components/Header';
 
 export default function AgendaPage() {
-  const { data: session, status } = useSession();
-
-  if (status === 'loading') return <p>Carregando...</p>;
-
-  // se não estiver logado, exibe botão de login
-  if (!session) {
-    return (
-      <div style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
-        <button onClick={() => signIn("google")}>
-          Entrar com Google
-        </button>
-      </div>
-    );
-  }
-
-  return <AgendaCalendar />;
+  return (
+    <ProtectedRoute>
+      <Header />
+      <AgendaCalendar />
+    </ProtectedRoute>
+  );
 }
