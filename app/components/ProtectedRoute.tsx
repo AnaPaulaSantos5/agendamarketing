838f74ee43c572b0a70f5314578687cff640251b
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-01 23:03:08 -0300

    Atualizar o ProtectedRoute.tsx

--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -1,4 +1,3 @@
-// app/components/ProtectedRoute.tsx
 'use client';
 
 import { useSession, signIn } from 'next-auth/react';
@@ -8,13 +7,11 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
   const { data: session, status } = useSession();
 
   useEffect(() => {
-    if (status === 'unauthenticated') {
-      signIn('google'); // redireciona para login
-    }
+    if (status === 'unauthenticated') signIn('google');
   }, [status]);
 
   if (status === 'loading') return <p>Carregando...</p>;
-  if (status === 'unauthenticated') return null; // enquanto redireciona
+  if (status === 'unauthenticated') return null;
 
   return <>{children}</>;
 }
