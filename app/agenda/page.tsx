2685dc82d14f0f8aaa6cdcbfba7ba10e3e673499
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-02 11:45:45 -0300

    Update page.tsx

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,12 +1,13 @@
 import Header from "@/app/components/Header";
-import ProtectedRoute from "@/app/components/ProtectedRoute";
 import AgendaCalendar from "@/app/components/AgendaCalendar";
 
 export default function AgendaPage() {
   return (
-    <ProtectedRoute>
+    <>
       <Header />
-      <AgendaCalendar />
-    </ProtectedRoute>
+      <main className="p-6">
+        <AgendaCalendar />
+      </main>
+    </>
   );
 }
