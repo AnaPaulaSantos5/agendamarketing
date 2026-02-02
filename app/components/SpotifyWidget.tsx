40323073b0da4931d5ea0b2aa0213adf005a4d1e
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-02 20:03:24 -0300

    Criar SpotifyWidget.tsx

---
+++ app/components/SpotifyWidget.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+export default function SpotifyWidget() {
+  return (
+    <div style={{ background: '#1DB954', color: '#fff', padding: 16, borderRadius: 8 }}>
+      <h4>Playlist Pública</h4>
+      <div style={{ height: 150, background: '#14833b', borderRadius: 4, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        Widget Spotify (template)
+      </div>
+    </div>
+  );
+}
