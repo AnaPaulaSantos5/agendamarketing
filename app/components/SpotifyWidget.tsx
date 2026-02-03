06f5f20b22210da187b9be49046c0790ad028dcd
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-02 21:13:51 -0300

    Atualizar o SpotifyWidget.tsx

--- app/components/SpotifyWidget.tsx
+++ app/components/SpotifyWidget.tsx
@@ -1,16 +1,8 @@
-"use client";
 import React from "react";
 import "./SpotifyWidget.css";
 
 const SpotifyWidget: React.FC = () => {
-  return (
-    <div className="spotify-widget">
-      <h4>Playlist Pública</h4>
-      <div className="spotify-placeholder">
-        <p>🎵 Widget do Spotify</p>
-      </div>
-    </div>
-  );
+  return <div className="spotify-widget">Spotify Widget</div>;
 };
 
 export default SpotifyWidget;
