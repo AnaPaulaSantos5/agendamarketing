ddc0b3a73afe0d8cca6dcf87d53c3d96e6606355
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-02 21:25:03 -0300

    Atualizar o WhatsAppWidget.tsx

--- app/components/WhatsAppWidget.tsx
+++ app/components/WhatsAppWidget.tsx
@@ -1,23 +1,9 @@
-// /app/components/WhatsAppWidget.tsx
-"use client";
+import React from 'react';
 
-import React from "react";
-
-const WhatsAppWidget: React.FC = () => {
-  return (
-    <div
-      style={{
-        padding: "10px",
-        background: "#25D366",
-        color: "#fff",
-        borderRadius: "5px",
-        textAlign: "center",
-        fontWeight: "bold",
-      }}
-    >
-      WhatsApp Widget
-    </div>
-  );
-};
+const WhatsAppWidget: React.FC = () => (
+  <div style={{ padding: "10px", background: "#25D366", color: "#fff", borderRadius: "5px", textAlign: "center", position: "fixed", bottom: "20px", right: "20px" }}>
+    WhatsApp Widget
+  </div>
+);
 
 export default WhatsAppWidget;
