bdff696fc5390b9e269d6317e62b42ebed07f853
AnaPaulaSantos5 <86023259+AnaPaulaSantos5@users.noreply.github.com>
2026-02-02 21:06:53 -0300

    Criar WhatsAppWidget.tsx

---
+++ app/components/WhatsAppWidget.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WhatsAppWidget: React.FC = () => {
+  return (
+    <div style={{ padding: "10px", background: "#25D366", color: "#fff", borderRadius: "5px", textAlign: "center" }}>
+      WhatsApp Widget
+    </div>
+  );
+};
+
+export default WhatsAppWidget;
